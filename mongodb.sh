0fa6f4c55d83295c362d7522437e353a3f108d53
Nagababu <nagababu.rachakonda@gmail.com>
2026-02-01 21:19:00 +0530

    adding file

--- mongodb.sh
+++ mongodb.sh
@@ -32,14 +32,13 @@ dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing Mongodb Server"
 
 systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enableing mongod"
+VALIDATE $? "Enableing mongodb"
 
 systemctl start mongod &>>$LOG_FILE
-VALIDATE $? "Starting mongod"
+VALIDATE $? "Starting mongodb"
 
 sed "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>>$LOG_FILE
 VALIDATE $? "Chaning IP address to 0.0.0.0"
 
 systemctl restart mongod &>>$LOG_FILE
-VALIDATE $? "Restarting Mongod"
-
+VALIDATE $? "Restarting Mongodb"
