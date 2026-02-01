9d1398b7b1b71ff49397ef2853c948e13329822b
Nagababu <nagababu.rachakonda@gmail.com>
2026-02-01 21:07:47 +0530

    adding file

---
+++ mongodb.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell_script/"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run the script through $R Root user" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ....$R FAILED" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 .....$G SUCCESS" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying mongo.repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing Mongodb Server"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enableing mongod"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "Starting mongod"
+
+sed "s/127.0.0.1/0.0.0.0/g" /etc/mongod.conf &>>$LOG_FILE
+VALIDATE $? "Chaning IP address to 0.0.0.0"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarting Mongod"
+
