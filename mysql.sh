7b4e86dba277f4062ed9f04f20af8f0c7e9ee060
Nagababu <nagababu.rachakonda@gmail.com>
2026-02-03 23:10:17 +0530

    adding the file

---
+++ mysql.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOG_FOLDER="/var/log/shell_script/"
+LOG_FILE="$LOG_FOLDER/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please login to root ruser $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOG_FOLDER
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2....$R FAILD $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2....$G SUCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf install mysql-server -y &>>LOG_FILE
+VALIDATE $? " Installing Mysql Server"
+
+systemctl enable mysqld
+VALIDATE $? "Enabling Mysqld service"
+
+systemctl start mysqld 
+VALIDATE $? "Starting the mysqld Service" 
