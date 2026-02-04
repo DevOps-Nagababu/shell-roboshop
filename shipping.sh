06a81279d9385c0a13ad75d744eaefed12d49c77
Nagababu <nagababu.rachakonda@gmail.com>
2026-02-04 12:58:29 +0530

    adding the file

--- shipping.sh
+++ shipping.sh
@@ -64,13 +64,13 @@ VALIDATE $? "Installing mysql client"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e "use cities"
 if [ $? -ne 0 ]; then
-        mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
+        mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql  &>>$LOG_FILE
         VALIDATE $? "Mysql Schema creating"
 
-        mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+        mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
         VALIDATE $? "Adding mysql app-user"
 
-        mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+        mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
         VALIDATE $? "Loading master data to mysql"   
 else    
         echo -e "data is already loaded ... $Y SKIPPING $N"
